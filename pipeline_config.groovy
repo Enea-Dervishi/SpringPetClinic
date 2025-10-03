7e5f87cd43851a8b08fc2e4ed223cd380162fa46
EneaDervishi <enea.dervishi@factor-y.com>
2025-10-03 08:34:54 +0200

    using jte terraform library

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,16 +1,29 @@
+// Pipeline configuration for SpringPetClinic using shared JTE terraform library
 libraries {
-    maven
-    git
-    sonarqube
-    splunk {
-        afterSteps = [ "static_code_analysis", "unit_test"  ]
+    terraform {
+        project_name = "petclinic"
+        version = "1.5"
+        container_name = "terraform"
+        auto_approve = false
+        allowed_environments = ['dev', 'staging', 'prod']
+        directory_pattern = "environments/\${environment}"
     }
-
 }
-stages {
-    continuous_integration {
-        build
-        static_code_analysis
+
+application_environments {
+    dev {
+        terraform_workspace = "dev"
+        working_directory = "terraform"
+        auto_approve = true  // Allow auto-approve for dev
     }
-    log version info
+    staging {
+        terraform_workspace = "staging"
+        working_directory = "terraform"
+        auto_approve = false
+    }
+    prod {
+        terraform_workspace = "prod"
+        working_directory = "terraform"
+        auto_approve = false  // Always require manual approval for prod
+    }
 }
