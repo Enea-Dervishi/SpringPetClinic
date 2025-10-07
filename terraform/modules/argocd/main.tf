0e3a7ded5f0c513555eabd7592a57f115fd116de
EneaDervishi <enea.dervishi@factor-y.com>
2025-10-07 16:12:50 +0200

    fix time sleep argocd

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -17,13 +17,7 @@ resource "kubernetes_namespace" "argocd" {
   
   lifecycle {
     ignore_changes = [metadata[0].annotations, metadata[0].labels]
-    prevent_destroy = true
   }
-}
-
-# Local value to reference the namespace regardless of how it was obtained
-locals {
-  argocd_namespace_name = try(data.kubernetes_namespace.argocd_existing.metadata[0].name, kubernetes_namespace.argocd[0].metadata[0].name)
 }
 
 # Install ArgoCD using the official manifests
@@ -31,20 +25,21 @@ data "http" "argocd_install" {
   url = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
 }
 
-# Apply ArgoCD manifests directly without splitting
+# Apply ArgoCD manifests
 resource "kubectl_manifest" "argocd_install" {
-  yaml_body = data.http.argocd_install.response_body
+  yaml_body          = data.http.argocd_install.response_body
   override_namespace = "argocd"
-  depends_on = [data.kubernetes_namespace.argocd_existing, kubernetes_namespace.argocd]
+  wait               = true
+  wait_for_rollout   = true
+  
+  depends_on = [kubernetes_namespace.argocd]
 }
 
-# Wait for ArgoCD server to be ready
-resource "null_resource" "wait_for_argocd" {
+# Wait a bit for ArgoCD to fully initialize
+resource "time_sleep" "wait_for_argocd" {
   depends_on = [kubectl_manifest.argocd_install]
   
-  provisioner "local-exec" {
-    command = "kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=600s"
-  }
+  create_duration = "30s"
 }
 
 resource "kubectl_manifest" "petclinic_application" {
@@ -78,7 +73,7 @@ resource "kubectl_manifest" "petclinic_application" {
     }
   })
   
-  depends_on = [null_resource.wait_for_argocd]
+  depends_on = [time_sleep.wait_for_argocd]
 }
 
 resource "kubernetes_service" "argocd_server_nodeport" {
@@ -93,7 +88,6 @@ resource "kubernetes_service" "argocd_server_nodeport" {
     port {
       port        = 80
       target_port = 8080
-      node_port   = var.argocd_node_port
       protocol    = "TCP"
       name        = "http"
     }
@@ -101,7 +95,6 @@ resource "kubernetes_service" "argocd_server_nodeport" {
     port {
       port        = 443
       target_port = 8080
-      node_port   = var.argocd_https_node_port
       protocol    = "TCP"
       name        = "https"
     }
