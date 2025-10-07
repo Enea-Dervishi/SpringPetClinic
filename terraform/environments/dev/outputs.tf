095c2bb4e0875397d1ee268dc4291b34cc25371a
EneaDervishi <enea.dervishi@factor-y.com>
2025-10-07 11:25:07 +0200

    Re-enable ArgoCD module in Terraform for JTE

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,18 +1,17 @@
-# ArgoCD outputs commented out since module is disabled
-# output "argocd_namespace" {
-#   description = "The ArgoCD namespace"
-#   value       = module.argocd.argocd_namespace
-# }
+output "argocd_namespace" {
+  description = "The ArgoCD namespace"
+  value       = module.argocd.argocd_namespace
+}
 
-# output "argocd_ui_url" {
-#   description = "ArgoCD UI URL"
-#   value       = module.argocd.argocd_ui_url
-# }
+output "argocd_ui_url" {
+  description = "ArgoCD UI URL"  
+  value       = module.argocd.argocd_ui_url
+}
 
-# output "application_name" {
-#   description = "ArgoCD Application name"
-#   value       = module.argocd.application_name
-# }
+output "application_name" {
+  description = "ArgoCD Application name"
+  value       = module.argocd.application_name
+}
 
 output "manifests_generated" {
   description = "Path where manifests were generated"
