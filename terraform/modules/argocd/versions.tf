5ca577420d2fa8c3e66696a11881e92475e21ecc
EneaDervishi <enea.dervishi@factor-y.com>
2025-10-07 14:16:03 +0200

    fix certificate

--- terraform/modules/argocd/versions.tf
+++ terraform/modules/argocd/versions.tf
@@ -22,13 +22,13 @@ terraform {
 }
 
 provider "kubernetes" {
-  host     = var.kubernetes_host
-  token    = var.kubernetes_token
-  insecure = true
+  host                   = var.kubernetes_host
+  token                  = var.kubernetes_token
+  cluster_ca_certificate = base64decode(var.kubernetes_ca_certificate)
 }
 
 provider "kubectl" {
-  host     = var.kubernetes_host
-  token    = var.kubernetes_token
-  insecure = true
+  host                   = var.kubernetes_host
+  token                  = var.kubernetes_token
+  cluster_ca_certificate = base64decode(var.kubernetes_ca_certificate)
 }
