e0fed2cae39c8fe809ff1578aed18ff3c4fcb2fa
EneaDervishi <enea.dervishi@factor-y.com>
2025-10-07 15:43:24 +0200

    fix k8s certificate

--- terraform/modules/argocd/versions.tf
+++ terraform/modules/argocd/versions.tf
@@ -28,7 +28,8 @@ provider "kubernetes" {
 }
 
 provider "kubectl" {
-  host     = var.kubernetes_host
-  token    = var.kubernetes_token
-  insecure = true
+  host                   = var.kubernetes_host
+  token                  = var.kubernetes_token
+  cluster_ca_certificate = base64decode(var.kubernetes_ca_certificate)
+  insecure               = true
 }
